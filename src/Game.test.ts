187d3d45305bd87d554f21d419936dc647025f43
Sam Hall <github@solflux.co.uk>
2023-04-28 23:04:37 +0100

    :red_circle: Added move token test case

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -12,4 +12,14 @@ describe("Moving your token", () => {
         // assert
         expect(tokenLocation).toBe(1);
     })
+
+    it("Given the token is on square 1, when the token is moved 3 spaces, then the token is on square 4", () => {
+        // arrange
+        const token = new Token("steve");
+        const game = new Game([token]);
+        // act
+        game.move(token, 3);
+        // assert
+        expect(game.getTokenLocation(token)).toBe(4);
+    })
 })
